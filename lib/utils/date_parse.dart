7924804ec4eef1045d17230a9c88f6965fe3b337
invokise <bilol586746@mail.ru>
2022-04-21 13:08:41 +0300

    Added method convertUnixtimeToDayOfTheWeek

--- lib/utils/date_parse.dart
+++ lib/utils/date_parse.dart
@@ -6,4 +6,10 @@ class DateParseUtil {
 
     return DateFormat.yMMMd().format(date);
   }
+
+  static String convertUnixTimeToDayOfTheWeek(int unixTime) {
+    var date = DateTime.fromMillisecondsSinceEpoch((unixTime * 1000));
+
+    return DateFormat.EEEE().format(date);
+  }
 }
